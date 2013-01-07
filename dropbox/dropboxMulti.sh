cf0f9f6f07be72e97358bc3c20f34e0087df9b80
David Lonjon <david.lonjon@enswer.net>
2013-01-07 14:32:29 +0900

    Add the start multio dropbox script

---
+++ dropbox/dropboxMulti.sh
@@ -0,0 +1,13 @@
+dropboxes=".dropbox_perso .dropbox_enswers"
+for dropbox in $dropboxes
+  do
+
+	HOME=/home/$USER
+	if ! [ -d $HOME/$dropbox ];then
+		mkdir $HOME/$dropbox #2> /dev/null
+		ln -s $HOME/.Xauthority $HOME/$dropbox/ #2> /dev/null
+      	fi
+
+	HOME=$HOME/$dropbox 
+	$HOME/.dropbox-dist/dropboxd & #2> /dev/null &
+  done
