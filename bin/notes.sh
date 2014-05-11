2c20955ae68df15d98b2136a8d56656c4567fd8c
David Lonjon <david.lonjon@gmail.com>
2014-05-11 17:16:03 +0900

    Add a new script to list all todo/hack/fixme/optimize notes in files

---
+++ bin/notes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Taken from https://github.com/pengwynn/dotfiles/blob/d698dac49246f38111a4c6f66cf04f639645e928/bin/notes
+if [[ -z $PATTERN ]]; then
+  PATTERN='TODO|HACK|FIXME|OPTIMIZE'
+fi
+
+gawk 'match($0, /^.*('$PATTERN'):?\s?(.*)$/, ary) {print FILENAME":"FNR":"ary[1]":"ary[2]}' $@
