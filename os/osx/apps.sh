f78c78b1edde3c7540ac7b0599ee56ea732074a0
David Lonjon <david.lonjon@gmail.com>
2015-08-03 09:03:26 +0900

    Add binaries and applications for provisionning

--- os/osx/apps.sh
+++ os/osx/apps.sh
@@ -4,36 +4,70 @@
 # Application installer (via brew-cask)
 #
 
-# Apps
-# apps=(
-#   dropbox
-#   google-chrome
-#   qlcolorcode
-#   slack
-#   appcleaner
-#   firefox
-#   hazel
-#   seil
-#   arq
-#   flash
-#   flux
-#   vlc
-#   cloudup
-#   skype
-#   transmission
-# )
+# General Apps
+apps=(
+  appcleaner
+  bartender
+  bettertouchtoo
+  # arq
+  cloudup
+  dropbox
+  flash
+  firefox
+  flux
+  google-chrome
+  google-drive
+  google-hangouts
+  libreoffice
+  # hazel
+  seil
+  skype
+  spectacle
+  teamViewer
+  timemachineeditor
+  toggldesktop
+  transmission
+  the-unarchiver
+  vlc
+  xtra-finder
+)
+
+design_apps=(
+  adobe-creative-cloud
+  slicy
+)
+
+# Apps installed  via the App store
+# Harvest
+# Dash
+# Aperture
+# Todoist
+# Slack
+# Twitter
+# Leaf
+# Pocket
+# Houdini
+# Simplenote
+# Snappy
+# Skitch
+# Kindle
 
 dev_apps=(
   apikitchen
   atom
+  cakebrew
+  charles
   dash
   filezilla
+  firefoxdeveloperedition
+  github
   google-chrome-canary
   iterm2
+  poedit
   qlcolorcode
   qlmarkdown
-  qlprettypatch
-  qlstephen
+  qlprettypatch # (https://github.com/atnan/QLPrettyPatch - QLPrettyPatch is a QuickLook generator for patch files.)
+  qlstephen # (http://whomwah.github.io/qlstephen/ - A QuickLook plugin that lets you view plain text files without a file extension)
   quicklook-json
   sublime-text3
   sequel-pro
@@ -52,8 +86,15 @@ fonts=(
 # Specify the location of the apps
 appdir="/Applications"
 
+# Install general apps
+e_header "Installing general applications via homebrew cask..."
+brew cask install --appdir=$appdir ${apps[@]}
 
-# Iinstall general apps
+# Install design apps
+e_header "Installing design applications via homebrew cask..."
+brew cask install --appdir=$appdir ${design_apps[@]}
+
+# Install development apps
 e_header "Installing development applications via homebrew cask..."
 brew cask install --appdir=$appdir ${dev_apps[@]}
 
